18c0acf55eb03663d516e756f7c71e209955e05b
Rafael Silva <r4faelrs@gmail.com>
2019-10-04 14:16:17 -0300

    Atualizacoes

--- Neps/dp - febre de recursoes.cpp
+++ Neps/dp - febre de recursoes.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 vector<long long> F(1010,-1);
-vector<int> A(1010,-1);
-int m,n,k;
-int find_f(int num){
+vector<long long> A(1010,-1);
+long long m,n,k;
+long long mod(long long x){
+    return(x%m+m)%m;
+}
+long long find_f(int num){
     if(F[num]!=-1) return F[num];
     long long f_aux=0;
     for(int i=1;i<=n;i++){
-        f_aux=(f_aux + A[i]*find_f(num-i)%m);
+        f_aux=mod(f_aux + A[i]*find_f(num-i));
     }
     return F[num]=f_aux;
 }
@@ -19,7 +22,13 @@ int main(){
     }
     for(int i=1;i<=n;i++){
         cin>>F[i];
+        F[i]=mod(F[i]);
     }
+    // for(int i=1;i<=n;i++){
+    //     if(F[k]!=0) break;
+    //     F[i]=(F[i]+A[i]*F[i-1])%m;
+    // }
+    // cout<<F[k]<<endl;
     cout<<find_f(k)<<endl;
     return 0;
 }
