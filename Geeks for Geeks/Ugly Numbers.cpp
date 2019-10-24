209de036092486243150d3b840b11eb51c8231c4
Rafael Silva <r4faelrs@gmail.com>
2019-10-24 16:39:49 -0300

    upd

---
+++ Geeks for Geeks/Ugly Numbers.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> dp(10010,-1);
+int ugly_number(int num){
+	
+
+}
+int main(){
+	int N;
+	for(int i=0;i<N;i++){
+		int aux;
+		cout<<ugly_number(aux)<<endl;
+	}
+	return 0;
+}
--- Neps/dp - trapalho papa.cpp
+++ Neps/dp - trapalho papa.cpp
@@ -2,28 +2,46 @@
 using namespace std;
 
 long long v[110],w[110];
+long long ans=0;
 long long N,W;
-long long MEM[1010][1010];
-long long dp(long long num,int i){
+//vector<vector<long long>> MEM(100001,vector<long long>(1001,100000));
+/*long long dp(long long num,int i){
     if(num<0 || i>=N){
         return 0;
     }
-    if(MEM[num][i]!=-1){
+    if(MEM[num][i]!=100000){
         return MEM[num][i];
     }
     long long aux=dp(num,i+1);
-    int res=min(-v[i]+w[i],num-v[i]+w[i]);
-    if(res>=0){
+    long long res=100010;
+    if(-v[i]+w[i]>0){
+        res=-v[i]+w[i];
+    }
+    if(num-v[i]>0){
+        res=min(res,num-v[i]);
+    }
+    if(num-v[i]<0 && -v[i]+w[i]<0){
+        res=aux;
+    }
+    if(res<100010 && res>=0){
         aux=max(1+dp(res,i+1),aux);
     }
     return MEM[num][i]=aux;
-}
+}*/
 int main(){
     cin>>N;
-    memset(MEM,-1,sizeof(MEM));
+    long long max_w=0;
+    int dp[1010];
+    memset(dp,-1,sizeof(dp));
     for(int i=0;i<N;i++){
         cin>>v[i]>>w[i];
     }
-    cout<<dp(0,0)<<endl;
+    for(int i=0;i<N;i++){
+        int atual_wheight=0;
+        
+    }
+  
+
+
     return 0;
 }
